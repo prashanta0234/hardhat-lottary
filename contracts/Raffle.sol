29686d515eeb44812cd83d566b1ebdc0ba616421
prashanta0234 <prashanta0234@gmail.com>
2022-07-27 22:19:47 +0600

    add event

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >= 0.7.0<0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Raffle {
-    uint256 privet minimunEth;
+    uint256 private minimunEth;
+    address payable[] private players;
+    error Please_Send_Minium_Eth();
+
+    // events
+    event raffle(address indexed player);
 
     constructor(uint256 ethValue) {
-        minimunEth=ethValue;
-        
+        minimunEth = ethValue;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < minimunEth) {
+            revert Please_Send_Minium_Eth();
+        }
+        players.push(payable(msg.sender));
+        emit raffle(msg.sender);
+    }
+
+    function getPlayers(uint256 index) public view {
+        players[index];
     }
 }
