be242080bf728e7e7f9780c449895cd2495df713
prashanta0234 <prashanta0234@gmail.com>
2022-07-31 01:16:50 +0600

    add fuldilrandom

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private minimunEth;
     address payable[] private players;
+    uint256[] public s_randomWords;
     error Please_Send_Minium_Eth();
 
     // events
     event raffle(address indexed player);
 
-    constructor(uint256 ethValue) {
+    constructor(address vrfCoordinator, uint256 ethValue) VRFConsumerBaseV2(vrfCoordinator) {
         minimunEth = ethValue;
     }
 
@@ -23,5 +26,14 @@ contract Raffle {
 
     function getPlayers(uint256 index) public view {
         players[index];
+    }
+
+    function requestRandomWords() external {}
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
     }
 }
